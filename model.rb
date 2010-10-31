7efa33c514d8cc36498e6437257bda012640ef10
KissCool <bayle.philippe@gmail.com>
2010-10-31 15:20:04 +0100

    fixed a bug between Bundler and cron jobs

--- model.rb
+++ model.rb
@@ -5,6 +5,7 @@ require 'rubygems'
 require 'socket'
 # use Bundler if present
 begin
+  ENV['BUNDLE_GEMFILE'] = File.join(File.dirname(__FILE__), './Gemfile')
   require 'bundler/setup'
 rescue LoadError
 end
